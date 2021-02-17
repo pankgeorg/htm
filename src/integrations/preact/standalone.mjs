81832b717c424e4a76589ba8be1708e63b078050
Panagiotis Georgakopoulos <panagiotis.georgakopoulos@intelligencia.ai>
2021-02-17 13:00:44 +0200

    Include debug in standalone

--- src/integrations/preact/standalone.mjs
+++ src/integrations/preact/standalone.mjs
@@ -10,7 +10,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+import 'preact/debug';
 import { h, Component, createContext, render } from 'preact';
 import { useState, useReducer, useEffect, useLayoutEffect, useRef, useImperativeHandle, useMemo, useCallback, useContext, useDebugValue, useErrorBoundary } from 'preact/hooks';
 import htm from '../../index.mjs';
